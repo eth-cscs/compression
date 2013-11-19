ebf3c2357595a2cf3b682dc54920cfdc6642f318
William <wsawyer@cscs.ch>
2013-11-19 16:30:09 +0100

    Changes ongoing

--- cxx/compression/netcdf_get_data.cpp
+++ cxx/compression/netcdf_get_data.cpp
@@ -25,8 +25,6 @@
 using namespace boost::numeric;
 
 #include "mpi.h"
-#include <netcdf_par.h>
-#include <netcdf.h>
 
 /**
 	   Write description of function here.
@@ -42,93 +40,7 @@ using namespace boost::numeric;
 	 */
 
 
-
-template <typename ScalarType>
-std::vector<ScalarType> netcdf_read_timeseries(const std::string filename, const std::vector<std::string> fields )
-{
-  int ncid, varid;
-  int ndims;
-
-  /* error handling */
-  int retval ;
-
-  int    *dimids;
-  size_t *dims;
-  size_t *p;
-  size_t *start, *count;
-  int my_rank, mpi_processes;
-  int slab_size ; // is the number of entries in one slab to be read in
-  ScalarType *data;
-
-  MPI_Comm comm = MPI_COMM_WORLD;
-  MPI_Info info = MPI_INFO_NULL;
-
-  MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );   
-  MPI_Comm_size( MPI_COMM_WORLD, &mpi_processes );
-
-  /* Open the file. NC_NOWRITE tells netCDF we want read-only access to the file.*/
-
-  std::cout << "Processing: " << filename.c_str() << " SEARCHING FOR " << fields[0].c_str() << std::endl;
-
-  if ((retval = nc_open_par(filename.c_str(), NC_NOWRITE|NC_MPIIO, comm, info, &ncid))) ERR(retval);
-  //  sequential: if ((retval = nc_open(filename.c_str(), NC_NOWRITE, &ncid))) ERR(retval);
-
-  if ((retval = nc_inq_varid(ncid, fields[0].c_str(), &varid))) ERR(retval);
-
-  if ((retval = nc_inq_varndims(ncid, varid, &ndims))) ERR(retval);
-  
-  std::cout << "ndims " << ndims << std::endl;
-
-  dimids = (int*)    malloc (sizeof(int)*ndims);
-  start  = (size_t*) malloc (sizeof(size_t)*ndims);   // Starting points for parallel implementation
-  count  = (size_t*) malloc (sizeof(size_t)*ndims);   // Slab sizes
-  dims   = (size_t*) malloc (sizeof(size_t)*ndims);
-
-  if ((retval = nc_inq_vardimid(ncid, varid, dimids))) ERR(retval);
-
-  p = dims;
-  for (int i=0; i<ndims; ++i ) {
-    if ((retval = nc_inq_dimlen(ncid, dimids[i], p))) ERR(retval);
-    std::cout << "dimension = " << *p++ << std::endl;
-    count[i] = dimids[i];
-    start[i] = 0;
-  }
-
-  if (dims[0] % mpi_processes != 0)
-    {
-      if (!my_rank) std::cout << "dims[0] " << dims[0] << " is not evenly divisible by mpi_size= " << mpi_processes << std::endl;
-      static std::vector<ScalarType> empty_vector;
-      return empty_vector;
-    }
-  
-  count[0] = dims[0] / mpi_processes;         // Number of 2D slices.  
-
-  slab_size = count[0];
-  for (int i=0; i<ndims; ++i ) { slab_size *= count[i]; }
-
-  data  = (ScalarType*) malloc (sizeof(ScalarType)*slab_size);
-
-  /* Read the slab this process is responsible for. */
-  start[0] = dims[0] / mpi_processes * my_rank;
-  std::cout << "Rank: " << my_rank << " reading slab: " << start[0] << std::endl;
-  /* Read one slab of data. */
-  if ((retval = nc_get_vara_double(ncid, varid, start, count, data))) ERR(retval);
-
-  std::vector<ScalarType> output(data, data+slab_size);
-
-  free( dims );
-  free( dimids );
-  free( start );
-  free( count );
-  free( data );
-
-  /* Close the file, freeing all resources. */
-  if ((retval = nc_close(ncid)))
-    ERR(retval);
-     
-  return output;
-}
-
+#include "read_timeseries.hpp"
 
 int main(int argc, char *argv[])
 //****************************************************************************80
@@ -258,7 +170,7 @@ int main(int argc, char *argv[])
   std::string              filename(argv[1]);
   std::vector<std::string> fields(argv+2, argv+argc);
 
-  std::vector<ScalarType>      X = netcdf_read_timeseries<ScalarType>( filename, fields);
+  std::vector<ScalarType>      X = read_timeseries<ScalarType>( filename, fields);
   viennacl::vector<ScalarType> Xvcl(X.size());
 
   viennacl::copy(X.begin(), X.end(), Xvcl.begin() );
