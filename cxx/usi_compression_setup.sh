dabcf7661c1b69da78cf869b88c7e6531d246874
Manuel Schmid <manuel.schmid@epfl.ch>
2014-07-10 14:50:56 +0200

    add file with all commands needed for compiling/running the cpp version

---
+++ cxx/usi_compression_setup.sh
@@ -0,0 +1,13 @@
+# This script sets up the modules and LD_LIBRARY_PATH needed
+# for compiling and running the usi_compression program.
+
+# load modules
+module swap PrgEnv-cray PrgEnv-gnu
+module load boost
+module load cmake
+module load craype-accel-nvidia35
+module load cray-netcdf-hdf5parallel
+module load cray-hdf5-parallel
+
+# add ld library path
+export LD_LIBRARY_PATH=/opt/intel/14.0.1.106/composer_xe_2013_sp1.1.106/mkl/lib/intel64:$LD_LIBRARY_PATH
