c99e640a98fea7c430a22014fe208dd660b3784d
Manuel Schmid <manuel.schmid@epfl.ch>
2014-07-14 09:46:21 +0200

    move unused minlin_wrappers.h to misc folder

--- cxx/compression/lanczos_correlation.h
+++ cxx/compression/lanczos_correlation.h
@@ -1,7 +1,4 @@
 #include "generic_wrappers.h"
-#if defined( USE_MINLIN )
-#include "minlin_wrappers.h"
-#endif
 
 template <typename ScalarType>
 bool lanczos_correlation(const GenericColMatrix &Xtranslated, const int ne, const ScalarType tol, const int max_iter, GenericColMatrix &EV, bool reorthogonalize = false );
