939c85a4a3ec143d1204c26f343e2bb59f490042
Manuel Schmid <manuel.schmid@epfl.ch>
2014-09-01 10:55:30 +0200

    fix computation of compressed matrix size

--- src/CompressedMatrix.h
+++ src/CompressedMatrix.h
@@ -166,7 +166,7 @@ private:
 
     // set up sizes
     original_size = Nc_ * Nd_total_;
-    compressed_size = Nd_total_ + Nc_ * K_ + K_ * Nc_ * M_ + K_ * M_ * Nd_total_;
+    compressed_size = Nd_total_ + Nc_ * K_ + K_ * Nc_ * M_ + M_ * Nd_total_;
   }
 
   /**
